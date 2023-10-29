e79b2261c04119b7351b1725af0806a43e867c89
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-29 15:07:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//your JS code here. If required.
+const container = document.querySelector('.container');
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+for (let i = 0; i < 800; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    
+   
+    square.style.backgroundColor = "black";
+    
+  
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = getRandomColor();
+    });
+	
+    square.addEventListener('mouseout', () => {
+        setTimeout(() => {
+            square.style.backgroundColor = getRandomColor();
+        }, 1000);
+
+        setTimeout(() => {
+          square.style.backgroundColor = "black";
+      }, 1000);
+      
+    });
+    
+    container.appendChild(square);
+}
